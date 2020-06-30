b0bcd38ffb97ea5b28c9de889df8db5b9e79f616
igomezpe <52471370+igomezpe@users.noreply.github.com>
2020-06-30 15:11:17 -0500

    Create index.php
    index

---
+++ index.php
@@ -0,0 +1,247 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <title>Ismael-Gomez</title>
+    <link rel="icon" type="image/x-icon" href="{{url_for('static',filename='favicon.ico')}}" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v5.13.0/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="{{url_for('static',filename='main.css')}}" >
+</head>
+<body>
+    <div class="container">
+        <div class="navbar">
+            <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+                <li class="nav-item" role="presentation">
+                  <a class="nav-link active" id="pills-about-tab" data-toggle="pill" href="#pills-about" role="tab" aria-controls="pills-about" aria-selected="true">About</a>
+                </li>
+                <li class="nav-item" role="presentation">
+                  <a class="nav-link" id="pills-experience-tab" data-toggle="pill" href="#pills-experience" role="tab" aria-controls="pills-experience" aria-selected="false">Experience</a>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <a class="nav-link" id="pills-education-tab" data-toggle="pill" href="#pills-education" role="tab" aria-controls="pills-education" aria-selected="false">Education</a>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <a class="nav-link" id="pills-skills-tab" data-toggle="pill" href="#pills-skills" role="tab" aria-controls="pills-skills" aria-selected="false">Skills</a>
+                </li>
+                <li class="nav-item" role="presentation">
+                  <a class="nav-link" id="pills-interests-tab" data-toggle="pill" href="#pills-interests" role="tab" aria-controls="pills-interests" aria-selected="false">Interests</a>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <a class="nav-link" id="pills-awards-tab" data-toggle="pill" href="#pills-awards" role="tab" aria-controls="pills-awards" aria-selected="false">Awards</a>
+                </li>
+              </ul>
+        </div>
+          <div class="tab-content" id="pills-tabContent">
+            <div class="tab-pane fade show active" id="pills-about" role="tabpanel" aria-labelledby="pills-about-tab">
+                 
+                <div class="jumbotron">
+                    <div class="row">
+
+
+                    <div class="col-md-4">
+                        <img class="profile rounded-circle" src="static/profile.jpg" alt="">
+                    </div>
+                    <div class="col-md-8">
+                        <h1 class="display-4" id="lblName">Ismael Avidan Gomez Perez</h1>
+                        <p class="lead" id="lblDescription">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+                        <hr class="my-4">
+                        <p id="lblAdress">It uses utility classes for typography and spacing to space content out within the larger container.</p>
+                        <ul class="list-group list-group-horizontal">
+                            <li class="list-group-item" id="lblDNI">DNI | 71995715</li>
+                            <li class="list-group-item" id="lblEmail">isgp@outlook.com.pe</li>
+                            <li class="list-group-item" id="lblPhone">965 498 295</li>
+                          </ul>
+                          <div class="social-icons mt-3">
+                            <a class="social-icon" href="#"><i class="fab fa-linkedin-in"></i></a>
+                            <a class="social-icon" href="#"><i class="fab fa-github"></i></a>
+                            <a class="social-icon" href="#"><i class="fab fa-twitter"></i></a>
+                            <a class="social-icon" href="#"><i class="fab fa-facebook-f"></i></a>
+                        </div>
+                      </div>
+                    </div>
+
+
+                </div>
+                <!-- About-->
+            </div>
+            <div class="tab-pane fade" id="pills-experience" role="tabpanel" aria-labelledby="pills-experience-tab">
+                            <!-- Experience-->
+                <section class="resume-section" id="experience">
+                    <div class="resume-section-content">
+                        <h2 class="mb-5">Experience</h2>
+                        <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                            <div class="flex-grow-1">
+                                <h3 class="mb-0">Senior Web Developer</h3>
+                                <div class="subheading mb-3">Intelitec Solutions</div>
+                                <p>Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring.</p>
+                            </div>
+                            <div class="flex-shrink-0"><span class="text-primary">March 2013 - Present</span></div>
+                        </div>
+                        <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                            <div class="flex-grow-1">
+                                <h3 class="mb-0">Web Developer</h3>
+                                <div class="subheading mb-3">Intelitec Solutions</div>
+                                <p>Capitalize on low hanging fruit to identify a ballpark value added activity to beta test. Override the digital divide with additional clickthroughs from DevOps. Nanotechnology immersion along the information highway will close the loop on focusing solely on the bottom line.</p>
+                            </div>
+                            <div class="flex-shrink-0"><span class="text-primary">December 2011 - March 2013</span></div>
+                        </div>
+                        <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                            <div class="flex-grow-1">
+                                <h3 class="mb-0">Junior Web Designer</h3>
+                                <div class="subheading mb-3">Shout! Media Productions</div>
+                                <p>Podcasting operational change management inside of workflows to establish a framework. Taking seamless key performance indicators offline to maximise the long tail. Keeping your eye on the ball while performing a deep dive on the start-up mentality to derive convergence on cross-platform integration.</p>
+                            </div>
+                            <div class="flex-shrink-0"><span class="text-primary">July 2010 - December 2011</span></div>
+                        </div>
+                        <div class="d-flex flex-column flex-md-row justify-content-between">
+                            <div class="flex-grow-1">
+                                <h3 class="mb-0">Web Design Intern</h3>
+                                <div class="subheading mb-3">Shout! Media Productions</div>
+                                <p>Collaboratively administrate empowered markets via plug-and-play networks. Dynamically procrastinate B2C users after installed base benefits. Dramatically visualize customer directed convergence without revolutionary ROI.</p>
+                            </div>
+                            <div class="flex-shrink-0"><span class="text-primary">September 2008 - June 2010</span></div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+            <div class="tab-pane fade" id="pills-education" role="tabpanel" aria-labelledby="pills-education-tab">
+                <!-- Education-->
+                <section class="resume-section" id="education">
+                    <div class="resume-section-content">
+                        <h2 class="mb-5">Education</h2>
+                        <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                            <div class="flex-grow-1">
+                                <h3 class="mb-0">University of Colorado Boulder</h3>
+                                <div class="subheading mb-3">Bachelor of Science</div>
+                                <div>Computer Science - Web Development Track</div>
+                                <p>GPA: 3.23</p>
+                            </div>
+                            <div class="flex-shrink-0"><span class="text-primary">August 2006 - May 2010</span></div>
+                        </div>
+                        <div class="d-flex flex-column flex-md-row justify-content-between">
+                            <div class="flex-grow-1">
+                                <h3 class="mb-0">James Buchanan High School</h3>
+                                <div class="subheading mb-3">Technology Magnet Program</div>
+                                <p>GPA: 3.56</p>
+                            </div>
+                            <div class="flex-shrink-0"><span class="text-primary">August 2002 - May 2006</span></div>
+                        </div>
+                    </div>
+                </section>
+
+            </div>
+            <div class="tab-pane fade" id="pills-skills" role="tabpanel" aria-labelledby="pills-skills-tab">
+                            <!-- Skills-->
+                <section class="resume-section" id="skills">
+                    <div class="resume-section-content">
+                        <h2 class="mb-5">Skills</h2>
+                        <div class="subheading mb-3">Programming Languages & Tools</div>
+                        <ul class="list-inline dev-icons">
+                            <li class="list-inline-item"><i class="fab fa-html5"></i></li>
+                            <li class="list-inline-item"><i class="fab fa-css3-alt"></i></li>
+                            <li class="list-inline-item"><i class="fab fa-js-square"></i></li>
+                            <li class="list-inline-item"><i class="fab fa-angular"></i></li>
+                            <li class="list-inline-item"><i class="fab fa-react"></i></li>
+                            <li class="list-inline-item"><i class="fab fa-node-js"></i></li>
+                            <li class="list-inline-item"><i class="fab fa-sass"></i></li>
+                            <li class="list-inline-item"><i class="fab fa-less"></i></li>
+                            <li class="list-inline-item"><i class="fab fa-wordpress"></i></li>
+                            <li class="list-inline-item"><i class="fab fa-gulp"></i></li>
+                            <li class="list-inline-item"><i class="fab fa-grunt"></i></li>
+                            <li class="list-inline-item"><i class="fab fa-npm"></i></li>
+                        </ul>
+                        <div class="subheading mb-3">Workflow</div>
+                        <ul class="fa-ul mb-0">
+                            <li>
+                                <span class="fa-li"><i class="fas fa-check"></i></span>
+                                Mobile-First, Responsive Design
+                            </li>
+                            <li>
+                                <span class="fa-li"><i class="fas fa-check"></i></span>
+                                Cross Browser Testing & Debugging
+                            </li>
+                            <li>
+                                <span class="fa-li"><i class="fas fa-check"></i></span>
+                                Cross Functional Teams
+                            </li>
+                            <li>
+                                <span class="fa-li"><i class="fas fa-check"></i></span>
+                                Agile Development & Scrum
+                            </li>
+                        </ul>
+                    </div>
+                </section>
+            </div>
+            <div class="tab-pane fade" id="pills-interests" role="tabpanel" aria-labelledby="pills-interests-tab">
+                <section class="resume-section" id="interests">
+                    <div class="resume-section-content">
+                        <h2 class="mb-5">Interests</h2>
+                        <p>Apart from being a web developer, I enjoy most of my time being outdoors. In the winter, I am an avid skier and novice ice climber. During the warmer months here in Colorado, I enjoy mountain biking, free climbing, and kayaking.</p>
+                        <p class="mb-0">When forced indoors, I follow a number of sci-fi and fantasy genre movies and television shows, I am an aspiring chef, and I spend a large amount of my free time exploring the latest technology advancements in the front-end web development world.</p>
+                    </div>
+                </section>
+            </div>
+            <div class="tab-pane fade" id="pills-awards" role="tabpanel" aria-labelledby="pills-awards-tab">
+                            <!-- Awards-->
+                <section class="resume-section" id="awards">
+                    <div class="resume-section-content">
+                        <h2 class="mb-5">Awards & Certifications</h2>
+                        <ul class="fa-ul mb-0">
+                            <li>
+                                <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                                Google Analytics Certified Developer
+                            </li>
+                            <li>
+                                <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                                Mobile Web Specialist - Google Certification
+                            </li>
+                            <li>
+                                <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                                1
+                                <sup>st</sup>
+                                Place - University of Colorado Boulder - Emerging Tech Competition 2009
+                            </li>
+                            <li>
+                                <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                                1
+                                <sup>st</sup>
+                                Place - University of Colorado Boulder - Adobe Creative Jam 2008 (UI Design Category)
+                            </li>
+                            <li>
+                                <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                                2
+                                <sup>nd</sup>
+                                Place - University of Colorado Boulder - Emerging Tech Competition 2008
+                            </li>
+                            <li>
+                                <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                                1
+                                <sup>st</sup>
+                                Place - James Buchanan High School - Hackathon 2006
+                            </li>
+                            <li>
+                                <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                                3
+                                <sup>rd</sup>
+                                Place - James Buchanan High School - Hackathon 2005
+                            </li>
+                        </ul>
+                    </div>
+                </section>
+            </div>
+          </div>
+    </div>
+
+<script src="{{url_for('static',filename='jquery.js')}}" ></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+<script src="{{url_for('static',filename='main.js')}}" ></script>
+<script src="{{url_for('static',filename='getData.js')}}" ></script>
+</body>
+</html>
